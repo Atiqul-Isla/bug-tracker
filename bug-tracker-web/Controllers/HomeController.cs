dcadcd158e737bacad4d298b0616d305d70d11a8
Atiqul Islam <isla.atiqul@gmail.com>
2023-05-25 17:42:24 -0400

    Added Dashboard

--- bug-tracker-web/Controllers/HomeController.cs
+++ bug-tracker-web/Controllers/HomeController.cs
@@ -1,21 +1,44 @@
 ﻿using bug_tracker_web.Models;
+using bug_tracker_web.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq;
 
 namespace bug_tracker_web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Get the data required for the dashboard
+            var totalProjects = _context.Projects.Count();
+            var totalBugs = _context.Bugs.Count();
+            var totalComments = _context.Comments.Count();
+            var bugsOverTime = _context.Bugs.OrderBy(b => b.BugCreatedAt).Select(b => b.BugCreatedAt).ToList();
+            var projectsWithMostBugs = _context.Projects.OrderByDescending(p => p.Bugs.Count).FirstOrDefault();
+            var personnel = _context.Users.ToList();
+
+            // Create the view model
+            var viewModel = new HomeViewModel
+            {
+                TotalProjects = totalProjects,
+                TotalBugs = totalBugs,
+                TotalComments = totalComments,
+                BugsOverTime = bugsOverTime,
+                ProjectsWithMostBugs = projectsWithMostBugs,
+                Personnel = personnel
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ bug-tracker-web/ViewModel/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using bug_tracker_web.Models;
+using System;
+using System.Collections.Generic;
+
+namespace bug_tracker_web.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Bug> Bugs { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<DefaultUser> Users { get; set; }
+        public List<BugUser> BugUsers { get; set; }
+        public List<ProjectUser> ProjectUsers { get; set; }
+
+        // Additional properties for the dashboard
+        public int TotalProjects { get; set; }
+        public int TotalBugs { get; set; }
+        public int TotalComments { get; set; }
+        public List<DateTime> BugsOverTime { get; set; }
+        public Project ProjectsWithMostBugs { get; set; }
+        public List<DefaultUser> Personnel { get; set; }
+    }
+}
